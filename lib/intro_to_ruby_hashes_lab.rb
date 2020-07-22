15e6bfe911b6a01da283343aa4b44712030ee7b1
Erik Zhao <erikzhao98@gmail.com>
2020-07-22 14:06:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  hash[:key] = value
+  hash[:key] = "hello"
 end
 #
 # def update_counting_hash(hash, key)
